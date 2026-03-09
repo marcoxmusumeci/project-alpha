282e11d2c41c0a7e28c3daaa55b7ebd3aa8b83ac
Marco <marco.musumeci@outlook.com>
2026-03-10 00:44:35 +0100

    Initial Commit

---
+++ project-alpha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-alpha
+//
+//  Created by AFP PAL 17 on 10/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-alpha/project_alphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_alphaApp.swift
+//  project-alpha
+//
+//  Created by AFP PAL 17 on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct project_alphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
